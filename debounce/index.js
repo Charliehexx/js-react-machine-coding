1dd3f424506d5b922260fd800ba3e58c4116783c
Charliehexx <adi2652002@gmail.com>
2024-01-03 20:46:38 +0530

    employee database

--- debounce/index.js
+++ debounce/index.js
@@ -1,5 +1,47 @@
-console.log("start");
-const sub = Promise.reject("subscribe to roadside coder");
-console.log(sub);
-sub.then((res) => console.log(res).catch((err) => console.log(err, "error")))
-console.log("Stop")
+const jwt = require('jsonwebtoken');
+const jwtPassword = "secret";
+const zod = require("zod");
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+function signJwt(username, password) {
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if (!usernameResponse.success || !passwordResponse.success) {
+        return null;
+    }
+
+    const signature = jwt.sign({
+        username
+    }, jwtPassword);
+
+    return signature;
+}
+
+function verifyJwt(token) {
+    let ans = true;
+    try {
+        jwt.verify(token, jwtPassword);
+    } catch (e) {
+        ans = false;
+    }
+    return ans;
+}
+
+function decodeJwt(token) {
+    // true, false
+    const decoded = jwt.decode(token);
+    if (decoded) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(signJwt("as@gmail.com", "ghjfjrtyurtyurt"))
+module.exports = {
+    signJwt,
+    verifyJwt,
+    decodeJwt,
+    jwtPassword
+}
